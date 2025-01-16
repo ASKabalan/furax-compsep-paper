293e893ad01f1065858cca7e02b480cf0ec380f8
Wassim Kabalan <wastondev@gmail.com>
2025-01-16 22:03:07 +0100

    update gitignore

---
+++ content/gridding.py
@@ -0,0 +1,196 @@
+import os
+
+os.environ["EQX_ON_ERROR"] = "nan"
+import jax
+import numpy as np
+from jax_grid_search import DistributedGridSearch
+
+from furax.comp_sep import (
+    spectral_cmb_variance,
+    get_clusters,
+    optimize,
+    negative_log_likelihood,
+    get_cutout_from_mask,
+)
+
+import jax.numpy as jnp
+
+from furax._base.core import HomothetyOperator
+from furax.landscapes import StokesPyTree
+from functools import partial
+from generate_maps import load_from_cache, save_to_cache
+
+
+import optax
+import lineax as lx
+from furax._base.core import Config
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Benchmark FGBuster and Furax Component Separation Methods"
+    )
+    
+    parser.add_argument(
+        "-n",
+        "--nside",
+        type=int,
+        default=16,
+        help="The nside of the map",
+    )
+    parser.add_argument(
+        "-c",
+        "--cache-run",
+        action="store_true",
+        help="Run the cache generation step",
+    )
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+    GAL020 = np.load("../notebooks/masks/GAL_PlanckMasks_64.npz")["GAL020"]
+    GAL040 = np.load("../notebooks/masks/GAL_PlanckMasks_64.npz")["GAL040"]
+    GAL060 = np.load("../notebooks/masks/GAL_PlanckMasks_64.npz")["GAL060"]
+
+    nside = args.nside
+    npixel = 12 * nside**2
+
+    if args.cache_run:
+        save_to_cache(nside, sky="c1d1s1")
+        return
+
+    nu, freq_maps = load_from_cache(nside, sky="c1d1s1")
+    # Check the shape of freq_maps
+    print("freq_maps shape:", freq_maps.shape)
+
+    (indices,) = jnp.where(GAL020 == 1)
+    freq_maps_m = np.zeros((freq_maps.shape[0] , freq_maps.shape[1] , len(indices)))
+    for i , _ in enumerate(freq_maps):
+        freq_maps_m[i , 1] = get_cutout_from_mask(freq_maps[i , 1]  , indices)
+
+
+    d = StokesPyTree.from_stokes(Q=freq_maps_m[:, 1, :], U=freq_maps_m[:, 2, :])
+
+    dust_nu0 = 150.0
+    synchrotron_nu0 = 20.0
+
+    spectral_cmb_variance_fn = partial(
+        spectral_cmb_variance, dust_nu0=dust_nu0, synchrotron_nu0=synchrotron_nu0
+    )
+    negative_log_likelihood_fn = partial(
+        negative_log_likelihood, dust_nu0=dust_nu0, synchrotron_nu0=synchrotron_nu0
+    )
+
+    N = HomothetyOperator(jnp.ones(1), _in_structure=d.structure)
+
+
+    solver = optax.lbfgs()
+
+    inverser_options = {
+        "solver": lx.CG(rtol=1e-6, atol=1e-6, max_steps=1000),
+        "solver_throw": False,
+    }
+
+    @partial(jax.jit, static_argnums=(5))
+    def compute_minimum_variance(
+        T_d_patches, B_d_patches, B_s_patches, planck_mask, indices, max_patches=25
+    ):
+        temp_dust_patch_indices = get_clusters(
+            planck_mask,
+            indices,
+            T_d_patches,
+            jax.random.PRNGKey(0),
+            max_centroids=max_patches,
+        )
+        beta_dust_patch_indices = get_clusters(
+            planck_mask,
+            indices,
+            B_d_patches,
+            jax.random.PRNGKey(0),
+            max_centroids=max_patches,
+        )
+        beta_pl_patch_indices = get_clusters(
+            planck_mask,
+            indices,
+            B_s_patches,
+            jax.random.PRNGKey(0),
+            max_centroids=max_patches,
+        )
+
+        params = {
+            "beta_dust": jnp.full((max_patches,), 1.54),
+            "temp_dust": jnp.full((max_patches,), 20.0),
+            "beta_pl": jnp.full((max_patches,), (-3.0)),
+        }
+
+        patch_indices = {
+            "temp_dust_patches": temp_dust_patch_indices,
+            "beta_dust_patches": beta_dust_patch_indices,
+            "beta_pl_patches": beta_pl_patch_indices,
+        }
+
+        masked_clusters = jax.tree.map(
+            lambda full_map: get_cutout_from_mask(full_map, indices).astype(jnp.int32),
+            patch_indices,
+        )
+
+        with Config(**inverser_options):
+            final_params, final_state = optimize(
+                params,
+                negative_log_likelihood_fn,
+                solver,
+                max_iter=500,
+                tol=1e-8,
+                verbose=False,
+                nu=nu,
+                N=N,
+                d=d,
+                patch_indices=masked_clusters,
+            )
+
+        value =  spectral_cmb_variance_fn(
+            final_params, nu=nu, d=d, N=N, patch_indices=masked_clusters
+        )
+
+        return {"value": value , "beta_dust": final_params["beta_dust"] , "temp_dust": final_params["temp_dust"] , "beta_pl": final_params["beta_pl"]}
+
+
+    max_centroids = 40
+
+
+    @jax.jit
+    def objective_function(T_d_patches, B_d_patches, B_s_patches):
+        return compute_minimum_variance(
+            T_d_patches,
+            B_d_patches,
+            B_s_patches,
+            GAL020,
+            indices,
+            max_patches=max_centroids,
+        )
+
+
+    # Put the good values for the grid search
+    search_space = {
+        "T_d_patches": jnp.arange(10, 40, 20),
+        "B_d_patches": jnp.arange(10, 40, 20),
+        "B_s_patches": jnp.arange(10, 40, 20),
+    }
+
+    grid_search = DistributedGridSearch(
+        objective_function, search_space, batch_size=2, progress_bar=True, log_every=0.1
+    )
+
+    results = grid_search.run()
+
+    # Save results
+    np.savez("results.npz", **results)
+
+
+
+
+if __name__ == "__main__":
+    main()
